5bef5443c0da8b96a326a239ddf2ab8d66522d13
karina Iramain <karina.iramain90@gmail.com>
2021-09-20 00:10:18 -0300

    ejercicio primera clase

--- statics/js/class1.js
+++ statics/js/class1.js
@@ -1,3 +1,21 @@
-function holaMundo() {
-    alert("Hola mundo!!!!");
-}
+var articulos = [];
+
+function classNumber1() {
+    let nombre = prompt("ingresa tu nombre");
+    let apellido = prompt("ingresa tu apellido");
+    let anioNacimiento = prompt("en que anio naciste?");
+    let mesNacimiento = prompt("En que mes naciste?");
+    let diaNacimiento = prompt("En que dia naciste?");
+    let year = new Date().getFullYear();
+    let fechaNacimiento = new Date(year, mesNacimiento - 1, diaNacimiento);
+    let now = new Date();
+    let edad = year - anioNacimiento;
+
+    if (fechaNacimiento > now) {
+        edad = edad - 1;
+
+    }
+    alert("Hola " + nombre + " " + apellido + " tu edad es: " + edad);
+
+
+};
