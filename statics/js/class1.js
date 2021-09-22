b6cda81e02bb8f29223ee173839aecc070e1a557
karina Iramain <karina.iramain90@gmail.com>
2021-09-22 19:05:28 -0300

    entregable clase 2

--- statics/js/class1.js
+++ statics/js/class1.js
@@ -1,5 +1,3 @@
-var articulos = [];
-
 function classNumber1() {
     let nombre = prompt("ingresa tu nombre");
     let apellido = prompt("ingresa tu apellido");
@@ -17,6 +15,28 @@ function classNumber1() {
 
     }
     alert("Hola " + nombre + " " + apellido + " tu edad es: " + edad);
+
+
+};
+
+
+function classNumber2() {
+    let nombre = prompt("ingresa tu nombre");
+    let apellido = prompt("ingresa tu apellido");
+    let DNI = prompt("ingresa tu DNI sin puntos");
+    let sueldo = prompt("ingrese su sueldo bruto mensual");
+    let sueldoMinimo = 40000;
+
+    if (sueldo >= sueldoMinimo) {
+
+        alert("Hola " + nombre + " " + apellido + " DNI " + DNI + " El monto maximo que podemos otorgarte es de: $" + sueldo * 3);
+
+
+    } else {
+        alert("lo sentimos en este momento no podemos otorgarte el credito")
+    }
+
+
 
 
 };
